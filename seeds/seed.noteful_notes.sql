40aba34da6adbdf290fc358f29c081a9ced1b67b
Tharwin <tharwin.carr08@gmail.com>
2020-10-21 15:32:19 -0700

    first commit

---
+++ seeds/seed.noteful_notes.sql
@@ -0,0 +1,7 @@
+INSERT INTO noteful_notes (name, folderId, content)
+VALUES
+('Random', 1, 'This is the first note'),
+('Bloc', 2, 'This is the second note'),
+('Thinkful', 3, 'This is the third note'),
+('Birds', 1, 'This is the fourth note'),
+('Dogs', 2, 'This is the fifth note');
